198f0f874ea1f1a3874ed7715a6dfdcfb8068b52
Jinge Zhang <jinge_windows@hotmail.com>
2024-01-14 22:14:37 +1100

    添加了一些注释

--- WinUI3_SelfLearning/Views/ShellPage.xaml.cs
+++ WinUI3_SelfLearning/Views/ShellPage.xaml.cs
@@ -30,16 +30,22 @@ public sealed partial class ShellPage : Page
         // TODO: Set the title bar icon by updating /Assets/WindowIcon.ico.
         // A custom title bar is required for full window theme and Mica support.
         // https://docs.microsoft.com/windows/apps/develop/title-bar?tabs=winui3#full-customization
+        //这里使用了自定义的TitileBar，也就是自定义应用程序顶部最大化最小化那一栏。
         App.MainWindow.ExtendsContentIntoTitleBar = true;
-        App.MainWindow.SetTitleBar(AppTitleBar);
+        App.MainWindow.SetTitleBar(AppTitleBar);   //这里的AppTitleBar是在XAML页面中定义的。
         App.MainWindow.Activated += MainWindow_Activated;
-        AppTitleBarText.Text = "AppDisplayName".GetLocalized();
+        AppTitleBarText.Text = "AppDisplayName".GetLocalized();  //这里的AppTitleBarText是在XAML页面中定义的TextBlock控件。
     }
 
+    /// <summary>
+    /// 该方法在页面加载的时候执行
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void OnLoaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
         TitleBarHelper.UpdateTitleBar(RequestedTheme);
-
+        //为当前的UI控件绑定键盘快捷键。这里绑定了导航的返回操作，键盘键分别为Alt+Left和鼠标上的GoBack。
         KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu));
         KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.GoBack));
     }
@@ -60,6 +66,12 @@ public sealed partial class ShellPage : Page
         };
     }
 
+    /// <summary>
+    /// 绑定键盘快捷键
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="modifiers">Ctrl、Alt、Shift这些修饰键</param>
+    /// <returns></returns>
     private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key, VirtualKeyModifiers? modifiers = null)
     {
         var keyboardAccelerator = new KeyboardAccelerator() { Key = key };
@@ -74,11 +86,16 @@ public sealed partial class ShellPage : Page
         return keyboardAccelerator;
     }
 
+    /// <summary>
+    /// 执行导航返回操作
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
     private static void OnKeyboardAcceleratorInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
     {
         var navigationService = App.GetService<INavigationService>();
 
-        var result = navigationService.GoBack();
+        bool result = navigationService.GoBack();
 
         args.Handled = result;
     }
